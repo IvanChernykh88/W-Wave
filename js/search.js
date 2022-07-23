2fc0471e98a388e88554f2ab16b0cc62c877f235
Ivan Chernykh <ivan.chernyh88@mail.ru>
2022-07-23 22:49:23 +0400

    Worked out everything except BEM

--- js/search.js
+++ js/search.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     var btn = document.querySelector('.search__btn');
+    btn.setAttribute("type", "button");
     console.log(btn);
-    btn.addEventListener('click', function() {
-        document.querySelector('.search__input').classList.add('search__input_active')
+    btn.addEventListener('click', function(e) {
+        setInterval(function() {
+            btn.setAttribute("type", "submit");
+        }, 200)
+        document.querySelector('.search__input').classList.add('search__input_active');
+        var input = document.querySelector('.search__input');
+        input.focus();
     })
 })
 
---
+++ js/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 4,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    loop: true,
+    spaceBetween: 30,
+
+  });
